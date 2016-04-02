d1c59670019d8b724c2353081c41640a950bc723
blckOS <blcksoftware@gmail.com>
2016-04-02 16:53:34 -0400

    Gradient Colors
    Added linearMap Function, lerpColors function and optional second color
    parameter for layer objects.

--- App.py
+++ App.py
@@ -1,16 +1,87 @@
 from Tkinter import *
 
+class layer(object):
+
+    def __init__(self,width,height,x=0,y=0,color1="blue",
+        color2=None):
+        self.width = width
+        self.height = height
+        self.color1 = color1
+        self.color2 = color2
+        self.x = x
+        self.y = y
+
+    def isClicked(self,x,y):
+        if (self.x < x < self.x + self.width and
+        self.y < y < self.y + self.height):
+            return True
+    
+    def isDraggable(self,boolVal):
+        pass
+
+    
+    @staticmethod  
+    def lerpColors(color1,color2,t):
+
+        #Algorithm from Wikipedia Article on Linear interpolation
+        lerp = lambda v0,v1,t: (1-t)*v0 + t*v1
+
+        r1,g1,b1 = color1
+        r2,g2,b2 = color2
+
+        r,g,b = lerp(r1,r2,t), lerp(g1,g2,t), lerp(b1,b2,t)
+
+        return (r,g,b)
+
+    #Algorith from
+    @staticmethod
+    def linearMap(x,A,B):
+        a0,a1 = A
+        b0,b1 = B
+        return b0 + (((x-a0)*b1-b0)/float(a1-a0))
+
+    def draw(self,canvas):
+        if (self.color2 != None):
+
+            x1,y1 = self.x, self.y
+            x2,y2 = self.x + self.width, self.y + self.height
+
+            A = (y1,y2)
+            B = (0,1)
+            index = y1
+
+            while index <= y2:
+                t = self.linearMap(index,A,B)
+                print t
+                r,g,b = self.lerpColors(self.color1,self.color2,t)
+                previousColor = (int(r),int(g),int(b))
+
+                color = "#%02x%02x%02x" % (int(r), int(g), int(b))
+                
+                p1 = (x1, y1+index)
+                p2 = (x2, y1+index)
+
+                canvas.create_line(p1, p2, fill = color)
+                index+=1
+
+        else:
+            p1 = (self.x,self.y)
+            p2 = (self.x + self.width, self.y + self.height)
+            canvas.create_rectangle(p1,p2,fill = self.color1)
+
+
 ####################################
 # customize these functions
 ####################################
 
 def init(data):
-    # load data.xyz as appropriate
+    black,white = (254,164,31),(254,87,46)
+    data.window = layer(300,400,50,50,black,white)
     pass
 
 def mousePressed(event, data):
     pass
-
+    
 def keyPressed(event, data):
     # use event.char and event.keysym
     pass
@@ -20,7 +91,7 @@ def timerFired(data):
 
 def redrawAll(canvas, data):
     drawDesktop(canvas,data)
-    pass
+    data.window.draw(canvas)
 
 def drawDesktop(canvas,data):
     canvas.create_rectangle(0,0,data.width+100,data.height+100,fill = "black")
